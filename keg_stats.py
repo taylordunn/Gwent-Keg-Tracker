06f6f3a897779329e2346b5f0425d47d94a5db18
taylordunn <taylor@tdunn.ca>
2017-07-02 20:55:49 -0300

    Added a plotting function to keg_stats

---
+++ keg_stats.py
@@ -0,0 +1,106 @@
+import numpy as np
+import pandas as pd
+import json
+import matplotlib.pylab as plt
+
+def load_cards(cards_json):
+    """
+    Opens the specified JSON file containing card details (name, group
+    and rarity). Returns a pandas dataframe of the data.
+    """
+    with open(cards_json) as infile:
+        cards = json.load(infile)
+        infile.close()
+    cards_dict = {}
+    cards_dict['name'] = [card['name'] for card in cards]
+    cards_dict['group'] = [card['group'] for card in cards]
+    cards_dict['rarity'] = [card['rarity'] for card in cards]
+    df = pd.DataFrame(cards_dict)
+    df.set_index('name', inplace=True)
+    return df
+
+#def plot_cards(cards_df):
+
+
+if __name__ == '__main__':
+    cards_df = load_cards('cards.json')
+    kegs_df = pd.read_csv('kegs_autoload.csv')
+    kegs_df = kegs_df[['date', 'card1', 'card2', 'card3', 'card4',
+                       'picked_card']]
+    n_kegs = len(kegs_df.index)
+    n_cards = 5 * n_kegs 
+    
+    rarity_counts = {}
+    premium_counts = {'total': 0}
+    for rarity in cards_df.rarity.unique():
+        rarity_counts[rarity] = 0
+        premium_counts[rarity] = 0
+
+    keg_number = []
+    counter = {}
+    counter['Rare'] = []
+    counter['Epic'] = []
+    counter['Legendary'] = []
+    kegs_df = kegs_df.reindex(index=kegs_df.index[::-1])
+    for row,(idx,keg) in enumerate(kegs_df.iterrows()):
+        print(row)
+        keg_number.append(row+1)
+        for rarity in ['Rare', 'Epic', 'Legendary']:
+            if row == 0: counter[rarity].append(0)
+            else: counter[rarity].append(counter[rarity][row-1])
+        for key in ['card1', 'card2', 'card3', 'card4', 'picked_card']:
+            card_name = keg.loc[key]
+            if '*' in card_name:
+                card_name = card_name.split('*')[0] 
+                card_rarity = cards_df.ix[card_name].rarity
+                premium_counts[card_rarity] += 1
+                premium_counts['total'] += 1
+            else:
+                card_rarity = cards_df.ix[card_name].rarity
+            rarity_counts[card_rarity] += 1
+            if card_rarity != 'Common': counter[card_rarity][-1] += 1
+    
+    print('Out of %d kegs (%d cards)' % (n_kegs, n_cards))
+    print('---------------------------------')
+    count = rarity_counts['Legendary']
+    for rarity in ['Legendary', 'Epic', 'Rare']:
+        count = rarity_counts[rarity]
+        perc = count / n_kegs
+        try: kegs_per = n_kegs / count
+        except: kegs_per = 0
+        print('{0}: {1} ({2:.2%} or 1 in {3:.1f} kegs)'.format(rarity, count,
+                                                         perc, kegs_per))
+    count = premium_counts['total']
+    perc = count / n_kegs
+    try: kegs_per = n_kegs / count
+    except: kegs_per = 0
+    print('---------------------------------')
+    print('Premiums: {0} ({1:.2%} or 1 in {2:.1f} kegs)'.format(count,
+                                                         perc, kegs_per))
+    for rarity in ['Legendary', 'Epic', 'Rare']:
+        count = premium_counts[rarity]
+        perc = count / n_kegs
+        try: kegs_per = n_kegs / count
+        except: kegs_per = 0
+        print('{0}: {1} ({2:.2%} or 1 in {3:.1f} kegs)'.format(rarity, count,
+                                                         perc, kegs_per))
+    
+
+    plt.plot(keg_number, counter['Legendary'],
+            color="#ffbf00", label='Legendary')
+    plt.plot(keg_number, counter['Epic'],
+            color="#663399", label='Epic')
+    x = np.arange(0, n_kegs, 0.1)
+    y1 = 0.2*x
+    y2 = 0.1*x
+    y3 = 0.05*x
+    plt.plot(x, y1, 'k-', label = "1 in 5 kegs")
+    plt.plot(x, y2, 'k--', label = "1 in 10 kegs")
+    plt.plot(x, y3, 'k:', label = "1 in 20 kegs")
+
+    plt.xlabel('Keg number')
+    plt.ylabel('Card count')
+    plt.legend()
+    plt.savefig('card_count.pdf')
+
+
