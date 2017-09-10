5f5fdefe68ce1a8afdff94ce7b20acda06e52563
taylordunn <taylor@tdunn.ca>
2017-09-10 13:03:23 -0300

    Updated keg_stats for latest patch and added new card database

--- keg_stats.py
+++ keg_stats.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import json
+import datetime
 import matplotlib.pylab as plt
 
 def load_cards(cards_json):
@@ -19,20 +20,24 @@ def load_cards(cards_json):
     df.set_index('name', inplace=True)
     return df
 
-#def plot_cards(cards_df):
-
 
 if __name__ == '__main__':
-    cards_df = load_cards('cards.json')
+    cards_df_current = load_cards('cards.json')
+
+    # Patch on August 30 added new cards, and renamed/reclassified old ones.
+    cards_df_170831 = load_cards('old/cards_170831.json')
+    date_170831 = pd.to_datetime("2017-08-30")
+
     kegs_df = pd.read_csv('kegs_autoload.csv')
     kegs_df = kegs_df[['date', 'card1', 'card2', 'card3', 'card4',
                        'picked_card']]
+    kegs_df.date = pd.to_datetime(kegs_df.date)
     n_kegs = len(kegs_df.index)
     n_cards = 5 * n_kegs 
     
     rarity_counts = {}
     premium_counts = {'total': 0}
-    for rarity in cards_df.rarity.unique():
+    for rarity in cards_df_current.rarity.unique():
         rarity_counts[rarity] = 0
         premium_counts[rarity] = 0
 
@@ -48,6 +53,15 @@ if __name__ == '__main__':
         for rarity in ['Rare', 'Epic', 'Legendary']:
             if row == 0: counter[rarity].append(0)
             else: counter[rarity].append(counter[rarity][row-1])
+        
+        # Determine which patch this keg was opened on, and assign the
+        #  appropriate card database
+        card_date = keg.loc['date']
+        if card_date < date_170831:
+            cards_df = cards_df_170831
+        else:
+            cards_df = cards_df_current
+
         for key in ['card1', 'card2', 'card3', 'card4', 'picked_card']:
             card_name = keg.loc[key]
             if '*' in card_name:
